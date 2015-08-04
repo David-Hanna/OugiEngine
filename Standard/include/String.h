b83c2d77af77b84be010736e5e5f7699bc110d82
David-Hanna <dhanna@upei.ca>
2015-08-04 00:32:49 -0300

    String Update

--- Standard/include/String.h
+++ Standard/include/String.h
@@ -23,32 +23,28 @@ namespace Ougi
 		
 		String Clone() const;
 		
-		char*       CString();
-		const char* CString() const;
+		const char const* CString() const;
 		
 		static unsigned int Length(const char cstring[]);
 		unsigned unsigned int Length() const;
 		bool Empty() const;
 		void Clear();
 		
-		char       operator[](const unsigned int index);
-		const char operator[](const unsigned int index) const;
-		char       CharAt(const unsigned int index);
-		const char CharAt(const unsigned int index) const;
-		char       Back();
-		const char Back() const;
-		char       Front();
-		const char Front() const;
+		char operator[](const unsigned int index) const;
+		char CharAt(const unsigned int index) const;
+		String Substring(const unsigned int start, const unsigned int end = ownLength) const;
+		
+		bool StartsWith(const char c) const;
+		bool StartsWith(const char cstring[]) const;
+		bool StartsWith(const String& other) const;
 		
 		bool Contains(const char c) const;
 		bool Contains(const char cstring[]) const;
 		bool Contains(const String& other) const;
 		
-		String Substring(const unsigned int start, const unsigned int end = length) const;
-		
-		String operator+(const char addend);
-		String operator+(const char addend[]);
-		String operator+(const String& addend);
+		String operator+(const char addend) const;
+		String operator+(const char addend[]) const;
+		String operator+(const String& addend) const;
 		
 		String& operator+=(const char addend);
 		String& operator+=(const char addend[]);
@@ -69,19 +65,36 @@ namespace Ougi
 		bool Replace(const char regex[]) const;
 		bool Replace(const String& regex) const;
 		
-		bool operator==(const char c);
-		bool operator==(const char cstring[]);
+		bool operator==(const char c) const;
+		bool operator==(const char cstring[]) const;
 		virtual bool operator==(const String& rhs) const override;
+		
+		bool operator!=(const char c) const;
+		bool operator!=(const char cstring[]) const;
+		
+		bool operator<(const char c) const;
+		bool operator<(const char cstring[]) const;
 		virtual bool operator<(const String& rhs) const override;
-	
-		protected:
-			unsigned int length;
-			char* data;
 			
-			void InitToEmpty();
-			void InitFromChar(const char c);
-			void InitFromCString(const char cstring[]);
-			void InitFromOther(const String& other);
+		bool operator<=(const char c) const;
+		bool operator<=(const char cstring[]) const;
+		
+		bool operator>(const char c) const;
+		bool operator>(const char cstring[]) const;
+		
+		bool operator>=(const char c) const;
+		bool operator>=(const char cstring[]) const;
+	
+	protected:
+		unsigned int ownLength;
+		char* data;
+		
+		void InitToEmpty();
+		void InitFromChar(const char c);
+		void InitFromCString(const char cstring[]);
+		void InitFromOther(const String& other);
+		
+		bool ContainsWithLength(const char cstring[], const unsigned int length) const;
 	};
 }
 
--- Standard/src/String.cpp
+++ Standard/src/String.cpp
@@ -22,28 +22,28 @@ Ougi::String::String(const String& other)
 
 void Ougi::String::InitToEmpty()
 {
+	ownLength = 0;
 	data = new char[1];
-	data[0] = '\0';
-	length = 0;
+	data[0] = 0;
 }
 
 void Ougi::String::InitFromChar(const char c)
 {
-	length = 1;
+	ownLength = 1;
 	data = new char[2];
 	data[0] = c;
-	data[1] = '\0';
+	data[1] = 0;
 }
 
 void Ougi::String::InitFromCString(const char cstring[])
 {
-	length = String.Length(cstring);
+	ownLength = String.Length(cstring);
 	data = new char[length + 1];
 	for (unsigned int i = 0; i < length; i++)
 	{
 		data[i] = cstring[i];
 	}
-	data[length] = '\0';
+	data[ownLength] = 0;
 }
 
 void Ougi::String::InitFromOther(const String& other)
@@ -66,15 +66,21 @@ Ougi::String& Ougi::String::operator=(const char c)
 
 Ougi::String& Ougi::String::operator=(const char cstring[])
 {
-	delete[] data;
-	InitFromCString(cstring);
+	if (data != cstring)
+	{
+		delete[] data;
+		InitFromCString(cstring);
+	}
 	return *this;
 }
 
 Ougi::String& Ougi::String::operator=(const String& rhs) 
 {
-	delete[] data;
-	InitFromOther(other);
+	if (this != &rhs)
+	{
+		delete[] data;
+		InitFromOther(other);
+	}
 	return *this;
 }
 
@@ -83,12 +89,7 @@ Ougi::String Ougi::String::Clone() const
 	return String(*this);
 }
 
-char* Ougi::String::CString()
-{
-	return data;
-}
-
-const char* Ougi::String::CString() const 
+const char const* Ougi::String::CString() const 
 {
 	return data;
 }
@@ -96,7 +97,7 @@ const char* Ougi::String::CString() const
 unsigned int Ougi::String::Length(const char cstring[])
 {
 	unsigned int length = 0;
-	for (unsigned int i = 0; cstring[i] != '\0'; i++)
+	for (unsigned int i = 0; cstring[i] != 0; i++)
 	{
 		length++;
 	}
@@ -105,12 +106,12 @@ unsigned int Ougi::String::Length(const char cstring[])
 
 unsigned int Ougi::String::Length() const
 {
-	return length;
+	return ownLength;
 }
 
 bool Ougi::String::Empty() const
 {
-	return length == 0;
+	return ownLength == 0;
 }
 
 void Ougi::String::Clear()
@@ -119,51 +120,55 @@ void Ougi::String::Clear()
 	InitToEmpty();
 }
 
-char Ougi::String::operator[](const unsigned int index)
+char Ougi::String::operator[](const unsigned int index) const
 {
-	return CharAt(index);
-}
-
-const char Ougi::String::operator[](const unsigned int index) const
-{
-	return CharAt(index);
-}
-
-char Ougi::String::CharAt(const unsigned int index)
-{
-	if (index >= length) return 0;
 	return data[index];
 }
 
-const char Ougi::String::CharAt(const unsigned int index) const
+char Ougi::String::CharAt(const unsigned int index) const
 {
-	if (index >= length) return 0;
 	return data[index];
 }
 
-char Ougi::String::Back()
+Ougi::String Ougi::String::Substring(const unsigned int start, const unsigned int end) const
 {
-	return data[length - 1];
+	const unsigned int length = end - start;
+	char* csubstring = new char[length];
+	for (unsigned int i = 0; i < length; ++i)
+	{
+		csubstring[i] = data[i + start];
+	}
+	String substring(csubstring);
+	delete[] csubstring;
+	return substring;
 }
 
-const char Ougi::String::Back() const
+bool Ougi::String::StartsWith(const char c) const
 {
-	return data[length - 1];
+	return data[0] == c;
+}
+bool Ougi::String::StartsWith(const char cstring[]) const
+{
+	if (cstring[0] == 0)
+	{
+		if (data[0] == 0) return true;
+		else return false;
+	}
+	for (unsigned int i = 0; cstring[i] != 0; ++i)
+	{
+		if (data[i] != cstring[i]) return false;
+	}
+	return true;
 }
 
-char Ougi::String::Front()
+bool Ougi::String::StartsWith(const String& other) const
 {
-	return data[0];
-}
-
-const char Ougi::String::Front() const
-{
-	return data[0];
+	return StartsWith(other.CString());
 }
 
 bool Ougi::String::Contains(const char c) const
 {
-	for (unsigned int = 0; i < length; ++i)
+	for (unsigned int = 0; i < ownLength; ++i)
 	{
 		if (data[i]) == c) return true;
 	}
@@ -172,7 +177,14 @@ bool Ougi::String::Contains(const char c) const
 
 bool Ougi::String::Contains(const char cstring[]) const
 {
-	// TODO
+	for (unsigned int i = 0; i < ownLength; ++i)
+	{
+		if (Substring(i, ownLength).StartsWith(cstring))
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 bool Ougi::String::Contains(const String& other) const
@@ -180,13 +192,48 @@ bool Ougi::String::Contains(const String& other) const
 	return Contains(other.CString());
 }
 
-Ougi::String Ougi::String::Substring(const unsigned int start, const unsigned int end = length) const;
+Ougi::String Ougi::String::operator+(const char addend) const
+{
+	char* cresult = new char[ownLength + 2];
+	for (unsigned int i = 0; i < ownLength; ++i)
+	{
+		cresult[i] = data[i];
+	}
+	cresult[ownLength] = addend;
+	cresult[ownLength + 1] = 0;
+	String result(cresult);
+	delete[] cresult;
+	return result;
+}
 
-Ougi::String Ougi::String::operator+(const char addend);
-Ougi::String Ougi::String::operator+(const char addend[]);
-Ougi::String Ougi::String::operator+(const String& addend);
+Ougi::String Ougi::String::operator+(const char addend[]) const
+{
+	const unsigned int addendLength = String.Length(addend);
+	char* cresult = new char[ownLength + addendLength + 1];
+	for (unsigned int i = 0; i < ownLength; ++i)
+	{
+		cresult[i] = data[i];
+	}
+	for (unsigned int i = 0; i < addendLength; ++i)
+	{
+		cresult[i + ownLength] = addend[i];
+	}
+	cresult[ownLength + addendLength] = 0;
+	String result(cresult);
+	delete[] cresult;
+	return result;
+}
 
-Ougi::String& Ougi::String::operator+=(const char addend);
+Ougi::String Ougi::String::operator+(const String& addend) const
+{
+	return (*this) + addend.CString();
+}
+
+Ougi::String& Ougi::String::operator+=(const char addend)
+{
+	
+}
+
 Ougi::String& Ougi::String::operator+=(const char addend[]);
 Ougi::String& Ougi::String::operator+=(const String& addend);
 
@@ -205,7 +252,22 @@ bool Ougi::String::Search(const String& regex) const;
 bool Ougi::String::Replace(const char regex[]) const;
 bool Ougi::String::Replace(const String& regex) const;
 
-bool Ougi::String::operator==(const char c);
-bool Ougi::String::operator==(const char cstring[]);
-virtual bool Ougi::String::operator==(const String& rhs) const override;
-virtual bool Ougi::String::operator<(const String& rhs) const override;
+bool Ougi::String::operator==(const char c) const
+{
+	return ownLength == 1 && data[0] == c;
+}
+
+bool Ougi::String::operator==(const char cstring[]) const
+{
+	return ownLength == String.Length(cstring) && StartsWith(cstring);
+}
+
+virtual bool Ougi::String::operator==(const String& rhs) const override
+{
+	return ownLength == rhs.Length() && StartsWith(rhs);
+}
+
+virtual bool Ougi::String::operator<(const String& rhs) const override
+{
+	
+}
